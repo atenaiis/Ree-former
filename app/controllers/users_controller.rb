d0c79a2766eb04f4b198f5f656c1764e5fb3a0b4
atenaiis <attennaisc@gmail.com>
2021-03-22 13:12:15 -0600

    create github repository

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,21 +1,40 @@
+# frozen_string_literal: true
+
 class UsersController < ApplicationController
-    def new
-        @user = User.new
+  def new
+    @user = User.new
+  end
+
+  def create
+    # @user = User.new(:username => params[:username], :email => params[:email], :password => params[:password])
+    @user = User.new(user_params)
+
+    if @user.save
+      flash[:success] = 'Successfully created'
+      redirect_to new_user_path
+    else
+      flash[:error] = 'oh oh! something went wrong'
+      render :new
     end
-    def create
-        @user = User.new(user_params)
-      
-        if @user.save
-          flash[:success] = 'Successfully created'
-          redirect_to new_user_path
-        else
-          flash[:error] = 'oh oh! something went wrong'
-          render :new
-        end
-      end
+    end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to edit_user_path
+    else
+      render 'edit'
+    end
+  end
 
       private
-      def user_params
-        params.require(:user).permit(:username,:email, :password)
-      end
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
     end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,10 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
-    validates :email,
+  validates :email,
             presence: true,
             uniqueness: true,
             format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
-    validates :username, presence: true, length: { in: 4..12 }, uniqueness: true
-    validates :password, presence: true
+  validates :username, presence: true, uniqueness: true
+  validates :password, presence: true
 end
